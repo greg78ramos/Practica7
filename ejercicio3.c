5b7df48e44b66858e787cafdf78f6bca5cd816d1
greg78ramos <gregoriomoralesramos17@gmail.com>
2026-03-26 13:50:11 -0600

    Update ejercicio3.c

--- ejercicio3.c
+++ ejercicio3.c
@@ -10,3 +10,16 @@ int main()
     printf("El número %d es impar.\n",num);
     return 0;
 }
+//CODIGO SIN CONDICIONAL
+#include <stdio.h>
+int main()
+{
+ int num;
+ printf("Ingrese un número:\n");
+ scanf("%d",&num);
+ if ( num%2 )
+ printf("El número %d es impar.\n",num);
+ else
+ printf("El número %d es par.\n",num);
+ return 0;
+}
